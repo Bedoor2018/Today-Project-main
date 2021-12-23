a212d76e4bb58e2be48affc2e52b0065f55482da
Bedoor Almutairi <badoor975@outlook.sa>
2021-12-23 12:28:37 +0300

    Categories articles

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 //var
-url1='https://newsapi.org/v2/everything?q=Apple&from=2021-12-19&sortBy=popularity&apiKey=22258211780d49d4ba1b1b80c8eda67e'
-url2='https://api.weatherbit.io/v2.0/current?city=Riyadh&key=23613dd34b1f4334a0280e3d2831a380';
+url1 =
+  "https://newsapi.org/v2/everything?q=all&from=2021-12-19&sortBy=popularity&apiKey=22258211780d49d4ba1b1b80c8eda67e";
+url2 =
+  "https://api.weatherbit.io/v2.0/current?city=Riyadh&key=23613dd34b1f4334a0280e3d2831a380";
 //selector
-const searchbut=document.querySelector('#searchCity');
-
-
+const searchbut = document.querySelector("#searchCity");
+const TechnologyCat = document.querySelector("#Technology");
+const HealthCat = document.querySelector("#Health");
+const AllCat = document.querySelector("#all");
+const BusinessCat = document.querySelector("#Business");
 //call functions
 getNews(url1);
 getWeather(url2);
@@ -12,44 +16,97 @@ getWeather(url2);
 //functions
 //------------------------------------------
 //1-function of get News information
-function  getNews(link){
-fetch(link)
-    .then(res=>res.json())
-    .then(data=> {
-       const html=data.articles.map(       
-                article=>{
-    
-                    return' <div class="card col-4" style="width: 18rem;"><img src="'+article.urlToImage+'" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+article.title+'</h5><p class="card-text">'+article.content+'</p><a href="'+article.url+'" class="btn btn-primary">read more</a></div></div>'      
-                }
-        ).join('');
-       document.querySelector('#app').innerHTML=html;
-
+function getNews(link) {
+  fetch(link)
+    .then(res => res.json())
+    .then(data => {
+      console;
+      const html = data.articles
+        .map(article => {
+          return (
+            ' <div class="card col-4" style="width: 18rem;"><img src="' +
+            article.urlToImage +
+            '" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">' +
+            article.title +
+            '</h5><p class="card-text">' +
+            article.content +
+            '</p><a href="' +
+            article.url +
+            '" class="btn btn-primary">read more</a></div></div>'
+          );
+        })
+        .join("");
+      document.querySelector("#app").innerHTML = html;
     })
-    .catch(error=>console.log('error'));
-};
+    .catch(error => console.log("error"));
+}
 
 //2-function of get weather information
-    function  getWeather(link){
-    fetch(link)
-    .then(res=>res.json())
-    .then(data=> {
-      console.log(data)
-        const html=data.data.map(
-            WeatherData=>{
-            document.querySelector('#city_name').innerHTML=WeatherData.city_name;
-            document.querySelector('#ob_time').innerHTML=WeatherData.ob_time;
-            document.querySelector('#temp').innerHTML=WeatherData.temp+" °C";
-
-            }
-        ).join('');
+function getWeather(link) {
+  fetch(link)
+    .then(res => res.json())
+    .then(data => {
+      const html = data.data
+        .map(WeatherData => {
+          document.querySelector("#city_name").innerHTML =
+            WeatherData.city_name;
+          document.querySelector("#ob_time").innerHTML = WeatherData.ob_time;
+          document.querySelector("#temp").innerHTML = WeatherData.temp + " °C";
+        })
+        .join("");
     })
-    .catch(error=>console.log('error'))
-    };
-
+    .catch(error => console.log("error"));
+}
 
 //event listener
- searchbut.addEventListener("click",function(){
- const searchInput= document.getElementById('searchCityInput');
- url3 = 'https://api.weatherbit.io/v2.0/current?city='+searchInput.value+'&key=23613dd34b1f4334a0280e3d2831a380';
- getWeather (url3);
- });
+searchbut.addEventListener("click", function() {
+  const searchInput = document.getElementById("searchCityInput");
+  url3 =
+    "https://api.weatherbit.io/v2.0/current?city=" +
+    searchInput.value +
+    "&key=23613dd34b1f4334a0280e3d2831a380";
+  getWeather(url3);
+});
+
+TechnologyCat.addEventListener("click", function() {
+    const activeButtons=document.querySelectorAll('.active');
+    activeButtons.forEach(element => {
+        element.classList.remove('active')
+    });
+    TechnologyCat.classList.add('active');
+  url3 =
+    "https://newsapi.org/v2/everything?q=Technology&from=2021-12-19&sortBy=popularity&apiKey=22258211780d49d4ba1b1b80c8eda67e";
+  getNews(url3);
+});
+
+HealthCat.addEventListener("click", function() {
+    const activeButtons=document.querySelectorAll('.active');
+    activeButtons.forEach(element => {
+        element.classList.remove('active')
+    });
+    HealthCat.classList.add('active');
+  url3 =
+    "https://newsapi.org/v2/everything?q=Health&from=2021-12-19&sortBy=popularity&apiKey=22258211780d49d4ba1b1b80c8eda67e";
+  getNews(url3);
+});
+
+AllCat.addEventListener("click", function() {
+    const activeButtons=document.querySelectorAll('.active');
+    activeButtons.forEach(element => {
+        element.classList.remove('active')
+    });
+    AllCat.classList.add('active');
+  url3 =
+    "https://newsapi.org/v2/everything?q=all&from=2021-12-19&sortBy=popularity&apiKey=22258211780d49d4ba1b1b80c8eda67e";
+  getNews(url3);
+});
+BusinessCat.addEventListener("click", function() {
+    const activeButtons=document.querySelectorAll('.active');
+    activeButtons.forEach(element => {
+        element.classList.remove('active')
+    });
+    BusinessCat.classList.add('active');
+    url3 =
+      "https://newsapi.org/v2/everything?q=Business&from=2021-12-19&sortBy=popularity&apiKey=22258211780d49d4ba1b1b80c8eda67e";
+    getNews(url3);
+  });
