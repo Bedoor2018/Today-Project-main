75293c6b7c5f831c291884f5a77cc41b4c3b76c3
Bedoor Almutairi <badoor975@outlook.sa>
2021-12-25 03:33:18 +0300

    article page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
 //var
 url1 =
-  "https://newsapi.org/v2/everything?q=all&from=2021-12-15&to=2021-12-19&sortBy=popularity&page=1&apiKey=2d850823b2fb4e028fb093a6502cec85";
+  "https://newsapi.org/v2/everything?q=all&from=2021-12-15&to=2021-12-19&sortBy=popularity&page=1&pageSize=5&apiKey=2d850823b2fb4e028fb093a6502cec85";
 url2 =
   "https://api.weatherbit.io/v2.0/current?city=Riyadh&key=23613dd34b1f4334a0280e3d2831a380";
 url4 =
-  "https://newsapi.org/v2/everything?q=all&from=2021-12-23&to=2021-12-25&sortBy=popularity&page=1&apiKey=2d850823b2fb4e028fb093a6502cec85";
+  "https://newsapi.org/v2/everything?q=all&from=2021-12-23&to=2021-12-25&sortBy=popularity&page=1&pageSize=3&apiKey=2d850823b2fb4e028fb093a6502cec85";
 
-  url5="https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyDCU6nXexQfVW0SoeXdz1URYFX6qadubWQ";//youtube api
 
 //selector
 const searchbut = document.querySelector("#searchCity");
@@ -15,7 +14,6 @@ const HealthCat = document.querySelector("#Health");
 const AllCat = document.querySelector("#all");
 const BusinessCat = document.querySelector("#Business");
 const SportCat = document.querySelector("#Sport");
-//const searchButton = document.querySelector("#searchButton");
 const one=document.querySelector('#one');
 const tow=document.querySelector('#tow');
 const three=document.querySelector('#three');
@@ -23,7 +21,6 @@ const three=document.querySelector('#three');
 getNews(url1);
 getWeather(url2);
 getLastNews(url4);
-
 //functions
 //------------------------------------------
 //1-function of get News information
@@ -35,21 +32,13 @@ function getNews(link) {
     .then(data => {
       console;
 data.articles.map(article => {
-          x++;
-          if(x<=4){
             z +='<div class="row"><div class="col-sm-4 grid-margin"><div class="position-relative"><div class="rotate-img"><img src="'+article.urlToImage+'" class="img-fluid"/></div><div class="badge-positioned"><span class="badge badge-danger font-weight-bold">Flash news</span></div></div></div><div class="col-sm-8  grid-margin"><h2 class="mb-2 font-weight-600">'+ article.title+'</h2><p class="mb-0">'+article.content +' </p><div class="fs-13 mb-2"><a class="mr-2" href="'+article.url+'">read more </a></div></div></div>';
-
-          }else{
-            z +="";
-
-          }
-        
         })
         .join("");
       document.querySelector("#app").innerHTML = z;
     })
     .catch(error => console.log("error"));
-}
+};
 
 //2-function of get weather information
 function getWeather(link) {
@@ -66,16 +55,11 @@ function getWeather(link) {
         .join("");
     })
     .catch(error => console.log("error"));
-}
-
-//3-function
-
-  function search(context) {
-
-    
 };
 
-//4-function of get laast News information
+
+
+//3-function of get laast News information
 function getLastNews(link) {
   var z=" <h2>Latest news</h2>";
   var x=0;
@@ -83,24 +67,18 @@ function getLastNews(link) {
     .then(res => res.json())
     .then(data => {
 data.articles.map(article => {
-          x++;
-          if(x<=3){
-            z += '<div class="d-flex border-bottom-blue pt-3 pb-4 align-items-center justify-content-between"><div class="pr-3"><h5>'+article.title+'</h5><div class="fs-12"><span class="mr-2">Photo </span>10 Minutes ago</div></div><div class="rotate-img"><img src="'+article.urlToImage+'"alt="thumb" class="img-fluid img-lg"/></div></div>';
-          }else{
-            z +="";
 
-          }
+            z += '<div class="d-flex border-bottom-blue pt-3 pb-4 align-items-center justify-content-between"><div class="pr-3"><h5>'+article.title+'</h5><div class="fs-12"><span class="mr-2">Photo </span>10 Minutes ago</div></div><div class="rotate-img"><img src="'+article.urlToImage+'"alt="thumb" class="img-fluid img-lg"/></div></div>';
         
         })
         .join("");
       document.querySelector("#lastNews").innerHTML = z;
     })
     .catch(error => console.log("error"));
-}
+};
 
 //event listener
 searchbut.addEventListener("click", function() {
-  const searchInput = document.getElementById("searchCityInput");
   url3 =
     "https://api.weatherbit.io/v2.0/current?city=" +
     searchInput.value +
@@ -116,7 +94,7 @@ TechnologyCat.addEventListener("click", function() {
     });
     TechnologyCat.classList.add('active');
   url3 =
-    "https://newsapi.org/v2/everything?q=Technology&from=2021-12-15&to=2021-12-19&sortBy=popularity&page="+ disabledButtons.value+"&apiKey=2d850823b2fb4e028fb093a6502cec85";
+    "https://newsapi.org/v2/everything?q=Technology&from=2021-12-15&to=2021-12-19&sortBy=popularity&page="+ disabledButtons.value+"&pageSize=5&apiKey=2d850823b2fb4e028fb093a6502cec85";
   getNews(url3);
 });
 
@@ -128,7 +106,7 @@ HealthCat.addEventListener("click", function() {
     });
     HealthCat.classList.add('active');
   url3 =
-    "https://newsapi.org/v2/everything?q=Health&from=2021-12-15&to=2021-12-19&sortBy=popularity&page="+ disabledButtons.value+"&apiKey=2d850823b2fb4e028fb093a6502cec85";
+    "https://newsapi.org/v2/everything?q=Health&from=2021-12-15&to=2021-12-19&sortBy=popularity&page="+ disabledButtons.value+"&pageSize=5&apiKey=2d850823b2fb4e028fb093a6502cec85";
   getNews(url3);
 });
 
@@ -140,7 +118,7 @@ AllCat.addEventListener("click", function() {
     });
     AllCat.classList.add('active');
   url3 =
-    "https://newsapi.org/v2/everything?q=all&from=2021-12-15&to=2021-12-19&sortBy=popularity&page="+ disabledButtons.value+"&apiKey=2d850823b2fb4e028fb093a6502cec85";
+    "https://newsapi.org/v2/everything?q=all&from=2021-12-15&to=2021-12-19&sortBy=popularity&page="+ disabledButtons.value+"&pageSize=5&apiKey=2d850823b2fb4e028fb093a6502cec85";
   getNews(url3);
 });
 BusinessCat.addEventListener("click", function() {
@@ -151,7 +129,7 @@ BusinessCat.addEventListener("click", function() {
     });
     BusinessCat.classList.add('active');
     url3 =
-      "https://newsapi.org/v2/everything?q=Business&from=2021-12-15&to=2021-12-19&sortBy=popularity&page="+ disabledButtons.value+"&apiKey=2d850823b2fb4e028fb093a6502cec85";
+      "https://newsapi.org/v2/everything?q=Business&from=2021-12-15&to=2021-12-19&sortBy=popularity&page="+ disabledButtons.value+"&pageSize=5&apiKey=2d850823b2fb4e028fb093a6502cec85";
     getNews(url3);
   });
 
@@ -164,14 +142,11 @@ BusinessCat.addEventListener("click", function() {
     });
     SportCat.classList.add('active');
     url3 =
-      "https://newsapi.org/v2/everything?q=Sport&from=2021-12-15&to=2021-12-19&sortBy=popularity&page="+ disabledButtons.value+"&apiKey=2d850823b2fb4e028fb093a6502cec85";
+      "https://newsapi.org/v2/everything?q=Sport&from=2021-12-15&to=2021-12-19&sortBy=popularity&page="+ disabledButtons.value+"&pageSize=5&apiKey=2d850823b2fb4e028fb093a6502cec85";
     getNews(url3);
   });
-  /*searchButton.addEventListener("click", function() {
-    const wordSearch = document.querySelector("#wordSearch");
-    search(wordSearch.value)
-  });
-*/
+
+
   one.addEventListener("click", function() {
     const disabledButtons=document.querySelectorAll('.disabled');
     disabledButtons.forEach(element => {
@@ -179,7 +154,7 @@ BusinessCat.addEventListener("click", function() {
     });
     one.classList.add('disabled');
     url3 =
-      "https://newsapi.org/v2/everything?q=all&from=2021-12-15&to=2021-12-19&sortBy=popularity&page=1&apiKey=2d850823b2fb4e028fb093a6502cec85";
+      "https://newsapi.org/v2/everything?q=all&from=2021-12-15&to=2021-12-19&sortBy=popularity&page=1&pageSize=5&apiKey=2d850823b2fb4e028fb093a6502cec85";
     getNews(url3);
   });
 
@@ -191,7 +166,7 @@ BusinessCat.addEventListener("click", function() {
     });
     tow.classList.add('disabled');
     url3 =
-      "https://newsapi.org/v2/everything?q=all&from=2021-12-15&to=2021-12-19&sortBy=popularity&page=2&apiKey=2d850823b2fb4e028fb093a6502cec85";
+      "https://newsapi.org/v2/everything?q=all&from=2021-12-15&to=2021-12-19&sortBy=popularity&page=2&pageSize=5&apiKey=2d850823b2fb4e028fb093a6502cec85";
     getNews(url3);
   });
 
@@ -202,6 +177,8 @@ BusinessCat.addEventListener("click", function() {
     });
     three.classList.add('disabled');
     url3 =
-      "https://newsapi.org/v2/everything?q=all&from=2021-12-15&to=2021-12-19&sortBy=popularity&page=3&apiKey=2d850823b2fb4e028fb093a6502cec85";
+      "https://newsapi.org/v2/everything?q=all&from=2021-12-15&to=2021-12-19&sortBy=popularity&page=3&pageSize=5&apiKey=2d850823b2fb4e028fb093a6502cec85";
     getNews(url3);
   });
+
+
